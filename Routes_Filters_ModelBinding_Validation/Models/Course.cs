c6667794672e6a1f485d4e0c6135ee19ec09d749
Gokul Shanth <gokul.shanth@gmail.com>
2020-04-10 12:17:16 -0400

    changes

---
+++ Routes_Filters_ModelBinding_Validation/Models/Course.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Routes_Filters_ModelBinding_Validation.Models
+{
+    public class Course
+    {
+        public string FirstName { get; set; }
+
+        public string CourseName { get; set; }
+    }
+}
